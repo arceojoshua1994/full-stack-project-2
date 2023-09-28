eab254e92be3cbfe48f9905a62b7b101bedcad72
arceojoshua94 <arceojoshua94@outlook.com>
2023-09-28 18:41:35 -0400

    fix routes to input data to webpages

--- controllers/mainRoutes.js
+++ controllers/mainRoutes.js
@@ -1,25 +1,58 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+  res.render('home');
+});
+
+router.get('/homepage', async (req, res) => {
+  res.render('homepage');
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+
+router.get('/popular-categories', (req, res) => {
+  res.render('popular-categories');
+});
+
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+router.get('/featured-recipes', (req, res) => {
+  res.render('featured-recipes');
+});
+
+router.get('/profile', (req, res) => {
+  res.render('profile');
+});
+
+
+// app.get('/form', (req, res) => {
+//   res.render('form'); 
+// });
+
+
+// app.post('/submit', (req, res) => {
+//   const data = req.body; 
+
+ 
+//   res.redirect('/success');
+// });
 
 //http://localhost:3001/
-router.get('/', (req, res) => {
-    const data = {
-      pageTitle: 'Welcome to Recipeasy!',
-      welcomeContent: [
-        {
-          paragraph: "Welcome to Recipeasy, your go-to platform for discovering and sharing delicious recipes from around the world!"
-        },
-        {
-          paragraph: "Whether you're a seasoned chef or just starting out in the kitchen, Recipeasy is here to make your cooking experience easy, fun, and full of flavor."
-        }
-      ]
-    };
+// router.get('/', (req, res) => {
+//     const data = {
+//       imageUrl: '/images/Carne-Asada-35.jpg' 
+//     };
   
-    res.render('homepage', data);
-  });
+//     res.render('main', data);
+//   });
 
 //http://localhost:3001/featured-recipes/
-router.get('/featured-recipes', (req, res) => {
+// router.get('/featured-recipes', (req, res) => {
   // const data = {
   //   featuredRecipes: [
   //     { 
@@ -50,39 +83,39 @@ router.get('/featured-recipes', (req, res) => {
   //   ]
   // };
 
-res.render('featured-recipes');
-});
+// res.render('featured-recipes');
+// });
 
 //http://localhost:3001/popular-categories
-router.get('/popular-categories', (req, res) => {   
-    const data = { 
-    popularCategories: [
-      { name: 'Latin', imageUrl: 'category1.jpg', id: 1 },
-      { name: 'Asian', imageUrl: 'category2.jpg', id: 2 },
-      { name: 'Caribbean', imageUrl: 'category3.jpg', id: 3},
-      { name: 'Mediterranean', imageUrl: 'category4.jpg', id: 4},
-      { name: 'American', imageUrl: 'category5.jpg', id: 5},
-    ],
-  };
-  res.render('popular-categories', data);
-});
+// router.get('/popular-categories', (req, res) => {   
+//     const data = { 
+//     popularCategories: [
+//       { name: 'Latin', imageUrl: 'category1.jpg', id: 1 },
+//       { name: 'Asian', imageUrl: 'category2.jpg', id: 2 },
+//       { name: 'Caribbean', imageUrl: 'category3.jpg', id: 3},
+//       { name: 'Mediterranean', imageUrl: 'category4.jpg', id: 4},
+//       { name: 'American', imageUrl: 'category5.jpg', id: 5},
+//     ],
+//   };
+//   res.render('popular-categories', data);
+// });
 
 //http://localhost:3001/about/
-router.get('/about', (req, res) => {
-  const data = {
-    pageTitle: 'About Us',
-    aboutContent: [
-          {
-            paragraph: "Recipeasy was founded in 2023 by Joshua Arceo and Juan Galvez in Miami, Florida. Our mission is to teach people how to cook a plethora of recipes from all around the world, regardless of age or background. We believe that cooking is an essential life skill that brings people together and allows for creativity and self-expression."
-          },
-          {
-            paragraph: "Our platform is designed as a social-media style recipe database, where users can share their favorite recipes, discover new ones, and connect with a community of passionate home cooks. Whether you're a seasoned chef or just starting out, Recipeasy is here to support you on your culinary journey."
-          }
-        ]
-      };
+// router.get('/about', (req, res) => {
+//   const data = {
+//     pageTitle: 'About Us',
+//     aboutContent: [
+//           {
+//             paragraph: "Recipeasy was founded in 2023 by Joshua Arceo and Juan Galvez in Miami, Florida. Our mission is to teach people how to cook a plethora of recipes from all around the world, regardless of age or background. We believe that cooking is an essential life skill that brings people together and allows for creativity and self-expression."
+//           },
+//           {
+//             paragraph: "Our platform is designed as a social-media style recipe database, where users can share their favorite recipes, discover new ones, and connect with a community of passionate home cooks. Whether you're a seasoned chef or just starting out, Recipeasy is here to support you on your culinary journey."
+//           }
+//         ]
+//       };
     
-      res.render('about', data);
-    });
+//       res.render('about', data);
+//     });
     
 
 module.exports = router;
