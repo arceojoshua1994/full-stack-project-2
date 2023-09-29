40a11a745f8157fdb1afedc2e1be53ebb663d54b
arceojoshua94 <arceojoshua94@outlook.com>
2023-09-29 18:55:46 -0400

    finish profile page

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
